053f38af45dcbd68443cfe4d866b63fa0b98defe
Philip Plagge <plaggep@gmail.com>
2022-05-05 22:12:08 +0200

    Initial commit

---
+++ packages/backend/config/database.ts
@@ -0,0 +1,16 @@
+export default ({ env }) => ({
+  connection: {
+    client: 'mysql',
+    connection: {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', ''),
+      user: env('DATABASE_USERNAME', ''),
+      password: env('DATABASE_PASSWORD', ''),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
+    },
+    debug: false,
+  },
+});
